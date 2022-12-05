bffa6960a8639de1078d660557a2a9f66ae2c28d
Patricio Drice <pdrice10@gmail.com>
2022-12-04 23:44:03 -0700

    Updated version of angular

--- src/app/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -4,34 +4,29 @@ import { DataService } from '../services/data.service';
 @Component({
   selector: 'app-pokemon-list',
   templateUrl: './pokemon-list.component.html',
-  styleUrls: ['./pokemon-list.component.css']
+  styleUrls: ['./pokemon-list.component.css'],
 })
 export class PokemonListComponent implements OnInit {
-
-  pokemons: any[] =[];
+  pokemons: any[] = [];
   page = 1;
-  totalPokemons:number;
+  totalPokemons: number;
 
-  constructor(
-    private dataService:DataService
-  ) { }
+  constructor(private dataService: DataService) {}
 
   ngOnInit(): void {
-   this.getPokemons();
+    this.getPokemons();
   }
-getPokemons(){
-  this.dataService.getPokemon(12,this.page + 0)
-  .subscribe((res:any)=>{
-    this.totalPokemons =res.count;
-    res.results.forEach(result=>{
-      this.dataService.getMoreData(result.name)
-      .subscribe((uniqueRes:any)=>{
-        this.pokemons.push(uniqueRes)
-          console.log(this.pokemons);
-          
-      })
+  getPokemons() {
+    this.dataService.getPokemon(12, this.page + 0).subscribe((res: any) => {
+      this.totalPokemons = res.count;
+      res.results.forEach((result) => {
+        this.dataService
+          .getMoreData(result.name)
+          .subscribe((uniqueRes: any) => {
+            this.pokemons.push(uniqueRes);
+            console.log(this.pokemons);
+          });
+      });
     });
-    
-  })
-}
+  }
 }
